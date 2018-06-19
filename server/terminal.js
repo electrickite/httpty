48f9e6820cacded0ff2facbec8395c84a41e091f
Corey Hinshaw <coreyhinshaw@gmail.com>
2018-06-18 20:21:49 -0400

    Store term as peroperty of socket to avoid memory leak

--- server/terminal.js
+++ server/terminal.js
@@ -1,41 +1,41 @@
 const pty = require('node-pty');
 
 module.exports = function(socket, opts) {
-  var opts = Object.assign({command: "/bin/sh", args: []}, opts);
+  opts = Object.assign({command: "/bin/sh", args: []}, opts);
 
   console.log('SID=%s CONNECTED', socket.id);
   if (opts.motd) { socket.emit('message', opts.motd); }
 
-  let term = pty.spawn(opts.command, opts.args, {
+  socket.term = pty.spawn(opts.command, opts.args, {
     name: 'xterm-256color',
     cols: 80,
     rows: 30
   });
-  console.log("PID=%s CREATED for SID=%s", term.pid, socket.id);
+  console.log("PID=%s CREATED for SID=%s", socket.term.pid, socket.id);
 
-  term.on('data', function(data) {
+  socket.term.on('data', function(data) {
     socket.emit('output', data);
   });
 
-  term.on('exit', function(code) {
+  socket.term.on('exit', function(code) {
     socket.emit('exit');
-    console.log("PID=%s EXITED", term.pid)
+    console.log("PID=%s EXITED", socket.term.pid)
   });
 
   socket.on('resize', function(data) {
-    if (term._writable) {
-      term.resize(data.col, data.row);
+    if (socket.term._writable) {
+      socket.term.resize(data.col, data.row);
     }
   });
 
   socket.on('input', function(data) {
-    if (term._writable) {
-      term.write(data);
+    if (socket.term._writable) {
+      socket.term.write(data);
     }
   });
 
   socket.on('disconnect', function() {
     console.log('SID=%s DISCONNECTED', socket.id);
-    term.end();
+    socket.term.end();
   });
 };
